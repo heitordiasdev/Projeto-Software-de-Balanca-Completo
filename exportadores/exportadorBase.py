91504d9bb972412ff8f626567d987ed4b7b7fc42
heitordiasdev <heitordiastavares3@gmail.com>
2023-10-26 20:16:20 -0300

    adding exceptions when entering a wrong price 2

--- exportadores/exportadorBase.py
+++ exportadores/exportadorBase.py
@@ -5,7 +5,14 @@ class Exporter:
     def export(self, products):
         with open(self.filename, 'w') as file:
             for product in products:
-                file.write(self.format_product(product) + '\n')
+                try:
+                    file.write(self.format_product(product) + '\n')
+                except ValueError as e:
+                    # Tratamento de exceção para preços inválidos
+                    print(f"Erro ao exportar o produto {product.code}: {e}")
+                except Exception as e:
+                    # Tratamento de exceção genérica
+                    print(f"Erro inesperado ao exportar o produto {product.code}: {e}")
 
     def format_product(self, product):
         pass
